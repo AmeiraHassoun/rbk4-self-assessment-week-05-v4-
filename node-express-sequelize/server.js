43399483156ea2588244a99ed07266b675d179ae
AmeiraHassoun <afhassoun5@gmail.com>
2018-04-14 09:58:52 +0300

    sequalize

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -9,10 +9,27 @@ var User = sequelize.define('User', {
 
 /*  Create a '/users' route that responds to 
     a GET request with all users in the database */
+app.get('/users',function(users){
+	
+	User.findAll().then(function (users) {
+		res.send(users)
+	})
 
+	// User.find(function(err,users){
+
+	// 	if(err){
+	// 		console.log(err)
+	// 	}else{
+	// 		res.send(users)
+	// 	}
+		
+	// })
+})
 
 
 module.exports = { 
   app: app,
   User: User
 };
+
+
